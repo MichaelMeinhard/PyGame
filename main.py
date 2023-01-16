3778f9f1e5f592ab504a67aaa2bb55b614e8a160
MichaelMeinhard <77785038+MichaelMeinhard@users.noreply.github.com>
2023-01-16 21:07:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from classes.hero import *
+
+pygame.init()
+
+bg_img = pygame.image.load("images/background.jpg")
+bg = pygame.transform.scale(bg_img,(1280, 720))
+
+def draw_game():
+    win.blit(bg, (0,0))
+    player.draw(win)
+    pygame.time.delay(30)
+    pygame.display.update()
+
+player = Hero(win_width/2, 525)
+
+# Main
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    userInput = pygame.key.get_pressed()
+
+    player.move_hero(userInput)
+    player.jump_motion(userInput)
+
+    draw_game()
